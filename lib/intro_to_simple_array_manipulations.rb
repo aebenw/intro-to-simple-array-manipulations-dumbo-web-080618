9989cc51d8b1713c70d66c5d5f84367611190eb2
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 05:34:34 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,25 +1,25 @@
 def using_push(array, string)
-  
+  array.push(string)
 end
 
 def  using_unshift(array, string)
-  
+  array.unshift(string)
 end
 
 def using_pop(array)
-  
+  array.pop
 end
 
 def pop_with_args(array)
-  
+  new = array.pop(array[-2,-1])
 end
 
 def using_shift(array)
-
+  array.shift
 end
 
 def shift_with_args(array)
-  
+  array.shift(array[0,1])
 end
 
 def using_concat(array1, array2)
@@ -38,6 +38,6 @@ def using_flatten(array, string)
   
 end
 
-def 
+def using_delete_at(array, int)
   
 end
